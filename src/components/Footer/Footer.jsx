095dc693b0bd44c272315e6f8ffba38e0284b283
mauri <mauricio.parodivar@gmail.com>
2026-08-29 13:01:09 -0300

    removing qr

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,8 @@
-import qrCode from '../../assets/qr-code.png'
 import './Footer.css'
 
 function Footer() {
   return (
     <footer className="footer">
-      <img className="footer-qr" src={qrCode} alt="Código QR del collar de Paquita" />
-      <p className="footer-qr-label">El QR de su collar 🐾</p>
       <p>Hecho con 💕 para Paquita</p>
     </footer>
   )
